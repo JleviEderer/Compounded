d1dd6e342376b3c3ef7f2907ca718363da0e1b1f
justin ederer <jleviederer@gmail.com>
2025-07-09 03:15:02 +0000

    Restored to '155618f7d614f9bda4d62b0efbf8032cb38d67f2'
    Replit-Restored-To: 155618f7d614f9bda4d62b0efbf8032cb38d67f2

--- client/src/pages/Insights.tsx
+++ client/src/pages/Insights.tsx
@@ -167,7 +167,6 @@ export default function Insights() {
 
       {dayModal && (
         <DayDetailModal 
-          key={`${dayModal}-${logs.length}`}
           date={dayModal} 
           onClose={() => setDayModal(null)} 
         />
